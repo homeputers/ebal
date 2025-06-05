4afc18108f283623bff7f51f6a09fb3a3d3101f7
Andres Rovira <wrovira@paypal.com>
2025-06-04 21:44:33 -0600

    fix admin user migration

--- backend/migrations/m230105_000012_insert_admin_user.php
+++ backend/migrations/m230105_000012_insert_admin_user.php
@@ -1,13 +1,12 @@
 <?php
 use yii\db\Migration;
-
 class m230105_000012_insert_admin_user extends Migration
 {
     public function safeUp()
     {
         $this->insert('{{%user}}', [
             'username' => 'admin',
-            'password_hash' => Yii::$app->security->generatePasswordHash('admin123'),
+            'password_hash' => \Yii::$app->security->generatePasswordHash('admin123'),
             'role' => 'admin',
         ]);
     }
